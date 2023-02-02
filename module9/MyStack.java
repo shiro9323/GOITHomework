796f00520551d6f2217ce3a7d4557acc70b84725
AndriiSh <trancelvania93@gmail.com>
2023-02-02 19:41:47 +0200

    new methods pop() and peek()

--- module9/MyStack.java
+++ module9/MyStack.java
@@ -1,4 +1,23 @@
 package module9;
 
-public class MyStack <E>{
+public class MyStack <T> extends MyArrayList<T>{
+
+    public void push(T value) {
+        super.add(value);
+    }
+
+    public T peek() {
+        return (T) arr[arr.length-1];
+    }
+
+    public T pop() {
+
+        int indexFifo = arr.length-1;
+        T firstElement = (T) arr[indexFifo];
+
+        remove(indexFifo);
+
+        return firstElement;
+
+    }
 }
