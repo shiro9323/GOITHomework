2ecca141fe89e438badcd18d9ae0f8d56dcbe704
AndriiSh <trancelvania93@gmail.com>
2023-02-06 16:56:52 +0200

    new class WordFrequency

---
+++ module10/WordFrequency.java
@@ -0,0 +1,47 @@
+package module10;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class WordFrequency {
+    public static void main(String[] args) {
+
+        Map<String, Integer> wordFrequency = new HashMap<>();
+        try (InputStream fis = new FileInputStream("Word.txt");
+             Scanner scanner = new Scanner(fis)) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] arrLine = line.split("\\s+");
+
+                for (String word : arrLine) {
+                    if(wordFrequency.containsKey(word)) {
+                        int counter = wordFrequency.get(word);
+                        counter++;
+                        wordFrequency.put(word, counter);
+                    } else {
+                        wordFrequency.put(word, 1);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(wordFrequency);
+
+
+        Comparator<String> comparator = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return wordFrequency.get(o2).compareTo(wordFrequency.get(o1));
+            }
+        };
+
+        Map<String, Integer> result = new TreeMap<>(comparator);
+        result.putAll(wordFrequency);
+
+        System.out.println(result);
+    }
+}
